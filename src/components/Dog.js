3e434bb43fab06132143598c744f4515fbd43333
harjitsingh3 <singh.harjit09@gmail.com>
2023-12-21 13:39:38 -0800

    Create Dog component

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -1,12 +1,15 @@
 import React from "react";
 
-function Dog() {
-    
-    
+const Dog = ({ dog }) => {
     return (
-        <>
-        </>
-    )
-}
+        <div className="dog">
+            <h3>{dog.name}</h3>
+            <img src={dog.img} alt={dog.name} />
+            <p>Breed: {dog.breed}</p>
+            <p>Age: {dog.age}</p>
+            <p>Zip Code: {dog.zip_code}</p>
+        </div>
+    );
+};
 
 export default Dog;
--- src/components/search.js
+++ src/components/search.js
@@ -1,90 +1,3 @@
-// import React, {useEffect, useState} from "react";
-// import Dog from "./Dog";
-
-// export default function Search() {
-
-//     // create local state variable for dogs
-//     const [dogs, setDogs] = useState([]);
-//     console.log(`Dog: ${dogs[0]}`);
-    
-//     const fetchDogs = async () => {
-//         // event.preventDefault();
-//         console.log('attempting to retrieve dogs');
-
-//         try {
-//             const response = await fetch('https://frontend-take-home-service.fetch.com/dogs/search', {
-//                 method: 'GET',
-//                 headers: {'Content-Type': 'application/json'},
-//                 credentials: 'include'
-//             })
-//             if (response.ok) {
-//                 const data = await response.json();
-//                 console.log('retrieved dogs successfully');
-//                 // data is an object with keys: next, resultIds, total. resultIds is an array of objects with key (num) and id (string)
-//                 // setDogs(data.resultIds);
-//                 console.log(data)
-                
-//                 // const dogIds = data.resultIds.map(obj => Object.values(obj));
-//                 const dogIds = Object.values(data.resultIds);
-//                 setDogs(dogIds);
-//             } else {console.log('Could not retrieve dogs')}
-            
-//         } catch (error) {
-//             console.log(error)
-//         }
-//     }
-
-//     useEffect(() => {
-//         fetchDogs();
-
-//         // fetch a single dog
-//         const fetchADog = async (dogs) => {
-//             console.log(`Dog: ${dogs}`)
-//             try {
-//             const response = await fetch('https://frontend-take-home-service.fetch.com/dogs', {
-//                 method: 'POST',
-//                 headers: {'Content-Type': 'application/json'},
-//                 body: JSON.stringify(dogs),
-//                 credentials: 'include'
-//             })
-//             if (response.ok) {
-//                 const data = await response.text();
-//                 console.log('retrieved single dog successfully');
-//                 console.log(data)
-//             } else {console.log('Could not retrieve single dog')}
-            
-//         } catch (error) {
-//             console.log(error)
-//         }
-//         }
-//         fetchADog();
-        
-//     }, [])
-    
-//     return (
-//         <>
-//             <h1>Search for your next dog!</h1>
-
-//             {/* <div className="dogs">
-//                 {dogs.map(dog => (
-//                     <div key={dog.id}>
-//                         {dog.name}
-//                         {dog.breed}
-//                         {dog.age}
-//                         {dog.zip_code}
-//                     </div>
-//                 ))}
-//             </div> */}
-
-//             <div>
-
-//             </div>
-
-//         </>
-//     )
-// }
-
-
 import React, { useEffect, useState } from "react";
 import Dog from "./Dog";
 
@@ -152,17 +65,9 @@ export default function Search() {
     return (
         <>
             <h1>Search for your next dog!</h1>
-
-            {/* Render dogs here */}
             <div className="dogs">
                 {dogs.map((dog, index) => (
-                    <div key={index}>
-                        {/* Display dog details */}
-                        Name: {dog.name}
-                        Breed: {dog.breed}
-                        Age: {dog.age}
-                       Zip Code: {dog.zip_code}
-                    </div>
+                     <Dog key={index} dog={dog}/>
                 ))}
             </div>
         </>
