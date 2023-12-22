a2287b21a425f91827369c5cc0a7a950115641c6
harjitsingh3 <singh.harjit09@gmail.com>
2023-12-21 17:37:58 -0800

    Add filter by breed functionality

--- src/components/Dog.js
+++ src/components/Dog.js
@@ -4,9 +4,9 @@ const Dog = ({ dog }) => {
     return (
         <div className="dog">
             <h3>{dog.name}</h3>
-            <img src={dog.img} alt={dog.name} />
-            <p>Breed: {dog.breed}</p>
+            {/* <img src={dog.img} alt={dog.name} /> */}
             <p>Age: {dog.age}</p>
+            <p>Breed: {dog.breed}</p>
             <p>Zip Code: {dog.zip_code}</p>
         </div>
     );
--- src/components/search.js
+++ src/components/search.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from "react";
 import Dog from "./Dog";
+// import TDog from "./TDog";
 
 export default function Search() {
     // create local state variable for dogs
     const [dogs, setDogs] = useState([]);
+
+    // array of dogs in local state
+    const [filteredDogs, setFilteredDogs] = useState([]);
+    // track state changes from user input of breed
+    const [breedFilter, setBreedFilter] = useState();
 
     // fetch details of each dog
     const fetchADog = async (dogIds) => {
@@ -20,7 +26,8 @@ export default function Search() {
 
             if (response.ok) {
                 const data = await response.json();
-                console.log('Retrieved single dog data successfully:', data);
+                console.log('Retrieved single dog data successfully');
+                // console.log(data);
                 return data;
             } else {
                 console.log('Could not retrieve single dog data');
@@ -34,21 +41,25 @@ export default function Search() {
     const fetchDogs = async () => {
         console.log('Attempting to retrieve dogs');
         try {
-            const response = await fetch('https://frontend-take-home-service.fetch.com/dogs/search', {
+            const response = await fetch('https://frontend-take-home-service.fetch.com/dogs/search?size=100', {
                 method: 'GET',
                 headers: {'Content-Type': 'application/json'},
+                // body: {size: 100},
                 credentials: 'include'
             });
 
             if (response.ok) {
                 const data = await response.json();
-                console.log('Retrieved dogs successfully:', data);
+                console.log('Retrieved dogs successfully:');
+                console.log(data);
                 
                 const dogIds = Object.values(data.resultIds).flat();
                 // make API call to fetchADog after we have created an array of ids
                 const dogDetails = await fetchADog(dogIds);
                 // Update the state with the dog details
                 setDogs(dogDetails); 
+                // Initially all dogs are displayed
+                setFilteredDogs(dogDetails);
             } else {
                 console.log('Could not retrieve dogs');
             }
@@ -62,12 +73,33 @@ export default function Search() {
         fetchDogs();
     }, []);
 
+    const handleBreedFilterChange = (event) => {
+        const selectedBreed = event.target.value;
+        setBreedFilter(selectedBreed);
+
+        // Filter the dogs based on the selected breed
+        const filtered = dogs.filter(dog => dog.breed === selectedBreed || selectedBreed === "");
+        setFilteredDogs(filtered);
+    };
+
     return (
         <>
             <h1>Search for your next dog!</h1>
+
+            <div>
+                <label htmlFor="breedFilter">Filter by Breed:</label>
+                <select id="breedFilter" value={breedFilter} onChange={handleBreedFilterChange}>
+                    <option value="">All Breeds</option>
+                    <option value="Chihuahua">Chihuahua</option>
+                    <option value="Silky Terrier">Silky Terrier</option>
+                    <option value="Soft-coated Wheaten Terrier">Soft-coated Wheaten Terrier</option>
+                </select>
+            </div>
+
             <div className="dogs">
-                {dogs.map((dog, index) => (
+                {filteredDogs.map((dog, index) => (
                      <Dog key={index} dog={dog}/>
+                    //  <TDog key={index} dog={dog}/>
                 ))}
             </div>
         </>
