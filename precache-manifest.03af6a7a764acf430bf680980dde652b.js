d2c509ab4d703bd1e8c01ef10178264e2390c3f2
harjitsingh3 <singh.harjit09@gmail.com>
2024-02-02 19:46:43 -0800

    Initial gh-pages commit

---
+++ precache-manifest.03af6a7a764acf430bf680980dde652b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c99d9b22c3d4be1071bb82c4225731f",
+    "url": "/adopt-a-dog/index.html"
+  },
+  {
+    "revision": "9b4621a4ced8ef9e55cc",
+    "url": "/adopt-a-dog/static/css/main.5f606f3a.chunk.css"
+  },
+  {
+    "revision": "5a97c945808e5fdf705d",
+    "url": "/adopt-a-dog/static/js/2.4c8f80d9.chunk.js"
+  },
+  {
+    "revision": "2a03a02ae21758236cc96053ed5846ed",
+    "url": "/adopt-a-dog/static/js/2.4c8f80d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb98c22cdede2f0bdf67",
+    "url": "/adopt-a-dog/static/js/3.f28749a6.chunk.js"
+  },
+  {
+    "revision": "9b4621a4ced8ef9e55cc",
+    "url": "/adopt-a-dog/static/js/main.3769898c.chunk.js"
+  },
+  {
+    "revision": "c867668531b2aca285d4",
+    "url": "/adopt-a-dog/static/js/runtime-main.14fb9610.js"
+  }
+]);
